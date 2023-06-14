c68799844992dba82326e37988daad316cf94468
Nigelms <almazchic@gmail.com>
2023-06-14 22:39:28 +0300

    add method getall and create db

---
+++ internal/repository/order_postgres.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"github.com/Nigelmes/L0/internal/models"
+	"github.com/jinzhu/gorm"
+)
+
+type OrderPostgres struct {
+	db *gorm.DB
+}
+
+func NewOrderPostgres(db *gorm.DB) *OrderPostgres {
+	return &OrderPostgres{db: db}
+}
+
+func (o *OrderPostgres) GetAll() ([]models.Order, error) {
+	var order []models.Order
+	err := o.db.Preload("Delivery").Preload("Payment").Preload("Items").Find(&order).Error
+	return order, err
+}
+
+func (o *OrderPostgres) Create(order models.Order) error {
+	err := o.db.Create(&order).Error
+	return err
+}
