0bae1a2db516d9f0fc390850749935ed5ea21fb7
Nigelms <almazchic@gmail.com>
2023-06-16 23:34:17 +0300

    настройки для сервера

---
+++ internal/server/server.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+	"context"
+	"github.com/Nigelmes/L0/internal/config"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(cfg *config.Config, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:           cfg.Server.Host + ":" + cfg.Server.Port,
+		Handler:        handler,
+		MaxHeaderBytes: 1 << 20, // 1 Mb
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) ShutDown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
